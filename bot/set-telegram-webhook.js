24a39d763e07d59e72d71f366946a59154d48484
Lautaro Alvarez <lautarolalvarez@gmail.com>
2019-05-05 12:29:42 -0300

    Saco logs. Configuro para que ni bien levanta haga get de eventos

--- bot/set-telegram-webhook.js
+++ bot/set-telegram-webhook.js
@@ -2,17 +2,13 @@
 const request = require('request-promise');
 
 function setTelegramWebhook() {
-  console.log(`${process.env.EXTERNAL_URL}${process.env.TELEGRAM_BOT_WEBHOOK_PATH}/`);
   return request({
     uri: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/setWebhook`,
     body: {
       url: `${process.env.EXTERNAL_URL}${process.env.TELEGRAM_BOT_WEBHOOK_PATH}/`
     },
     json: true
-  })
-    .then((res) => {
-      console.log(res);
-    });
+  });
 }
 
 module.exports = setTelegramWebhook;
--- data-loader/schedule.js
+++ data-loader/schedule.js
@@ -3,6 +3,7 @@ const schedule = require('node-schedule');
 const updateData = require('./update-data');
 
 function initSchedule() {
+  updateData();
   schedule.scheduleJob('3 */1 * *', () => {
     updateData();
   });
